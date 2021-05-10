2074986078f707ca19823d71c3770b2e5aa4de9c
rofe <github@rofe.com>
2021-05-10 08:21:00 +0200

    feat: multiple gallery blocks per page

--- js/gallery.js
+++ js/gallery.js
@@ -12,44 +12,41 @@ function link($elem, href, cl) {
 window.addEventListener('DOMContentLoaded', () => {
   window.gallery.observer.disconnect();
 
-  const $gallery = document.querySelector('.gallery');
-  if (!$gallery) {
-    return;
-  }
+  document.querySelectorAll('.gallery').forEach(($gallery) => {
+    // wrap thumbnails with links to the full version
+    const $gallerySection = $gallery.closest('.section-wrapper');
+    $gallery.querySelectorAll('picture').forEach(($pic) => {
+      const $img = $pic.querySelector('img');
+      const fullUrl = $img.src
+        .replace(/\?width\=\d+/, '?width=2000') // increase width to 2000px
+        .replace(/\&crop\=\d+:\d+/,''); // remove cropping
+      const $thumb = link($img, fullUrl, 'thumb');
+      $pic.parentElement.appendChild($thumb);
+      $pic.remove();
+    });
+    
+    const sections = Array.from(document.querySelectorAll('.section-wrapper'));
+    const hasHeader = sections[0] && sections[0].classList.contains('hero');
+    if (hasHeader) {
+      // add gallery below title if header section
+      sections[0].appendChild($gallery);
+    }
 
-  // wrap thumbnails with links to the full version
-  const $gallerySection = $gallery.closest('.section-wrapper');
-  $gallery.querySelectorAll('picture').forEach(($pic) => {
-    const $img = $pic.querySelector('img');
-    const fullUrl = $img.src
-      .replace(/\?width\=\d+/, '?width=2000') // increase width to 2000px
-      .replace(/\&crop\=\d+:\d+/,''); // remove cropping
-    const $thumb = link($img, fullUrl, 'thumb');
-    $pic.parentElement.appendChild($thumb);
-    $pic.remove();
-  });
-  
-  const sections = Array.from(document.querySelectorAll('.section-wrapper'));
-  const hasHeader = sections[0] && sections[0].classList.contains('hero');
-  if (hasHeader) {
-    // add gallery below title if header section
-    sections[0].appendChild($gallery);
-  }
+    // treat last section as epilog
+    const $epilogSection = sections.pop();
+    if ($epilogSection !== $gallerySection) {
+      $epilogSection.classList.add('epilog');
+    }
 
-  // treat last section as epilog
-  const $epilogSection = sections.pop();
-  if ($epilogSection !== $gallerySection) {
-    $epilogSection.classList.add('epilog');
-  }
+    // find wall div and initialize lightgallery
+    const $wall = $gallery.querySelector('.gallery > div > div');
+    // remove everything but links from wall div
+    $wall.querySelectorAll('a').forEach(($a) => $wall.append($a));
+    $wall.querySelectorAll(':scope > *:not(a)').forEach(($el) => $el.remove());
 
-  // find wall div and initialize lightgallery
-  const $wall = $gallery.querySelector('.gallery > div > div');
-  // remove everything but links from wall div
-  $wall.querySelectorAll('a').forEach(($a) => $wall.append($a));
-  $wall.querySelectorAll(':scope > *:not(a)').forEach(($el) => $el.remove());
-
-  $wall.classList.add('gallery-wall');
-  lightGallery($wall, {
-    thumbnail: true,
+    $wall.classList.add('gallery-wall');
+    lightGallery($wall, {
+      thumbnail: true,
+    });
   });
 });
